8f37fd2fba460d69c3f9ca463b14c9cfa23923e3
masaques <msaques@itba.edu.ar>
2015-12-18 20:24:45 -0300

    Initial commit

---
+++ desktop/src/com/mygdx/gravitytoy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.gravitytoy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.gravitytoy.Toy;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Toy(), config);
+	}
+}
